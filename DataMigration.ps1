29a4e023f03c3b22976ef2946bd218ec710dddf6
Torben Schou <tschou@solidq.com>
2021-10-26 15:07:17 +0200

    Update DataMigration.ps1
    Ensure only assessment against newer target - (On-premise)

--- DataMigration.ps1
+++ DataMigration.ps1
@@ -50,7 +50,7 @@ Clear-Host
 $Global:file = $ErrorReport
 
 #region Verify that Microsoft Data Migration Tool are installed
-Function Is-Installed
+Function Is-Installed ()
 {
 
   try
@@ -73,7 +73,7 @@ Function Is-Installed
     break;
   }
 
-  $WorkDir = Get-ChildItem -path 'C:\Program Files' -Recurse -Include dmacmd.exe | Select -ExpandProperty Directory
+  $WorkDir = Get-ChildItem -path 'C:\Program Files' -Recurse -Include dmacmd.exe | Select-Object -ExpandProperty Directory
 
   return $WorkDir
 
@@ -102,47 +102,46 @@ Function GetDatabasesOnInstance([string] $SQLInstance)
   if (Get-module -ListAvailable -name SQLSERVER) 
   {  
     try
-	{
+    {
       $flag = $true
-	  Write-Progress "Retrieving databases from SQL Server Instance $SQLInstance - using SQL Module ...."
-	  WriteToLog "Retrieving databases from SQL Server Instance $SQLInstance - using SQL Module ...."
-	  $mydatabases = Get-SqlInstance -ServerInstance $SQLInstance -ErrorAction Stop   | Get-SqlDatabase | Where-Object { $_.ID -gt 4 } | SELECT name
-	}
-	catch 
-	{
-	  $flag = $false
-	  $message = "Failed on retieving database information from $SQLInstance `n" 
-	  $message += "$_.Exception.message "
-	  WriteToLog $message
-
-	  Break;
+      Write-Progress "Retrieving databases from SQL Server Instance $SQLInstance - using SQL Module ...."
+      WriteToLog "Retrieving databases from SQL Server Instance $SQLInstance - using SQL Module ...."
+      $mydatabases = Get-SqlInstance -ServerInstance $SQLInstance -ErrorAction Stop   | Get-SqlDatabase | Where-Object { $_.ID -gt 4 } | Select-Object name
+    }
+    catch 
+    {
+      $flag = $false
+      $ex = $_.Exception
+      $message = "ERROR - Failed on retieving database information from $SQLInstance `n" 
+      $message += $ex.message 
+      WriteToLog $message
     }
     
-  } elseif (Get-module -ListAvailable -name SQLPS) 
+  } 
+    elseif (Get-module -ListAvailable -name SQLPS) 
   {  	
 	try 
-	{ 
-	  $flag= $true
-	 
-	  Write-Progress "Retrieving databases from SQL Server Instance $SQLInstance - using SQLPS ...."
-	  WriteToLog "Retrieving databases from SQL Server Instance $SQLInstance - using SQLPS ...."
-	  
-	  $srv = New-Object 'Microsoft.SqlServer.Management.SMO.Server' $SQLInstance
-	  $mydatabases = $srv.Databases | where ID -GT 4 | select name	
-	} 
-	catch
-	{
-	  $flag = $false
-      $message = $_.Exception.message + ' -  ' + $SQLInstance
-	  WriteToLog $message
-	}
+    { 
+      $flag= $true    
+      Write-Progress "Retrieving databases from SQL Server Instance $SQLInstance - using SQLPS ...."
+      WriteToLog "INFO - Retrieving databases from SQL Server Instance $SQLInstance - using SQLPS ...."
+      $srv = New-Object 'Microsoft.SqlServer.Management.SMO.Server' $SQLInstance
+      $mydatabases = $srv.Databases | Where-Object ID -GT 4 | Select-Object name	
+    } 
+    catch
+    {
+      $flag = $false
+      $ex = $_.Exception
+      $message = $SQLInstance + " `n"
+      $message = $ex.message
+      WriteToLog $message
+    }
   }
 	
-
   if ($mydatabases.Count -eq 0)
   {
-     Write-Progress "No databases could be found in SQL Instance $SQLInstance"
-     $message = "No databases could be found in SQL Instance $SQLInstance `n"
+     Write-Progress "WARNING - No databases could be found in SQL Instance $SQLInstance"
+     $message = "WARNING - No databases could be found in SQL Instance $SQLInstance `n"
      WriteToLog $message	 
 	 
   } else {
@@ -172,27 +171,58 @@ Function DatabasesArray([string]$SQLInstance)
 
   foreach ($i IN $GetDatabasesOnInstance)
   {
-
     $DatabaseName = $i.Name.ToString()
-
     $Connection += '"Server='+$SQLInstance+';Initial Catalog='+$DatabaseName+';Integrated Security=true;"'
-
     $Connection = $Connection + ' '
   }
-
   #$Connection = '"' + $Connection
-
   return $Connection
 }
 #endregion Build database Array
 
+Function GetDbTargetVersion ([string] $mySQLInstance, [string] $myTarget)
+{
+    #Validating of Instance version against requested target
+    switch ($myTarget)
+    {   
+        "SqlServer2016" 
+        {      
+            $ver = 13
+        }
+        "SqlServerWindows2017" 
+        {      
+            $ver = 14
+        }
+        "SqlServerWindows2019" 
+        {      
+            $ver = 15
+        }
+    }
+
+    $myVer = (Get-SqlInstance -ServerInstance $mySQLInstance).VersionMajor
+
+    if ($Ver -lt $myver)
+    {
+        $message = "WARNING - Will not assess migration to a lower version of existing $SQLInstance - build $myVer`n" 
+        WriteToLog $message        
+        Break;
+    } else {
+        $message = "INFO - $SQLInstance Will be assess for migration to $myTarget`n" 
+        WriteToLog $message
+    }    
+}
+
 $WorkDir = Is-Installed
+
+if ($Target -in "SqlServer2016","SqlServerWindows2017","SqlServerWindows2019")
+{
+    GetDbTargetVersion $SQLInstance $Target 
+}
 
 $DatabasesArray = DatabasesArray $SQLInstance
 
 if ($DatabasesArray.Count -gt 0)
 {
-
     $SQLInstance = $SQLInstance.Replace("\", "_")
 
     switch ($output)
@@ -208,7 +238,6 @@ if ($DatabasesArray.Count -gt 0)
           '/AssessmentResultJson="' + $ResultOutputPath + $SQLInstance + "_" + $Target +'.json" '
         )
       }
-  
       "CSV"
       {
         $ArgList = @(
@@ -234,7 +263,6 @@ if ($DatabasesArray.Count -gt 0)
       }
 
     }
-
 
     Start-Process "dmacmd.exe" -ArgumentList $ArgList -WorkingDirectory $WorkDir
 
